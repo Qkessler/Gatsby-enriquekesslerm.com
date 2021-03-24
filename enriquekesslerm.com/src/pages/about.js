98b444c923afb92c3a7f8e8e2a7a7ddb8580d2d9
Enrique Kessler Martínez <qkessler@gmail.com>
2021-03-24 18:24:49 +0100

    Adding start of about page

--- enriquekesslerm.com/src/pages/about.js
+++ enriquekesslerm.com/src/pages/about.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Container } from 'theme-ui'
 import Layout from 'gatsby-theme-blorg/src/components/layout'
 
 export default function AboutPage() {
   return (
     <Layout>
-      <h1>About page</h1>
+      <Container variant='content'>
+        <main sx={{ flex: 1, pb: 4, mx: 'auto'}}>
+          <h1>About page</h1>
+        </main>
+      </Container>
     </Layout>
   )
 }
