cf545c922c967803cbb80c117f945c4bb6c85f29
Enrique Kessler Martínez <qkessler@gmail.com>
2021-03-26 17:47:22 +0100

    Changing the structure for the project-show component

--- enriquekesslerm.com/src/components/project-show.js
+++ enriquekesslerm.com/src/components/project-show.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Box, Heading, Text } from 'theme-ui'
 
 
-export default function PostShow({ logo, name, description }) {
-  return (
-    <div>
-      <Box as='div' p={2} my={3} mx={2} sx={{ border: '1px solid black' }
-      }>
-        <Heading as='h4' > {name} </Heading>
-        <Text> {description} </Text>
-      </Box>
-    </div>
-  )
-}
+const PostShow = ({ logo, name, description }) => (
+  <div>
+    <Box as='div' p={2} my={3} mx={2} sx={{ border: '1px solid black' }
+    }>
+      <Heading as='h4' > {name} </Heading>
+      <Text> {description} </Text>
+    </Box>
+  </div>
+)
+
+export default PostShow;
