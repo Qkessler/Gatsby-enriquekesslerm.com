6eeb577d25e0ce5d6bfee2f0d95b323e229f3c66
Enrique Kessler Martínez <qkessler@gmail.com>
2021-03-26 17:27:19 +0100

    Adding working rainbow stripe, trying to make the text use it

--- enriquekesslerm.com/src/pages/about.js
+++ enriquekesslerm.com/src/pages/about.js
@@ -2,7 +2,12 @@ import React from 'react'
 import { Container, Text, Heading, Box } from 'theme-ui'
 import Layout from 'gatsby-theme-blorg/src/components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+
 import ProjectShow from '../components/project-show'
+import MagicRainbowText from '../components/moving-rainbow-text'
+
+
+const aboutMeText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna lacus, vestibulum at fermentum a, fermentum ut arcu. Phasellus eget velit est. Vivamus id fringilla ligula, quis pulvinar orci. Morbi dignissim dui libero, at varius justo mollis in. Pellentesque suscipit vitae lorem a volutpat. Phasellus mattis sagittis erat quis mollis.'
 
 export default function AboutPage() {
   return (
@@ -11,18 +16,18 @@ export default function AboutPage() {
         <main sx={{ flex: 1, pb: 4, mx: 'auto' }}>
           TODO: Will have to change the Enrique part to gradient.
           <Heading as='h1' sx={{ pb: 4 }}>Hey, It's Enrique</Heading>
-          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna lacus, vestibulum at fermentum a, fermentum ut arcu. Phasellus eget velit est. Vivamus id fringilla ligula, quis pulvinar orci. Morbi dignissim dui libero, at varius justo mollis in. Pellentesque suscipit vitae lorem a volutpat. Phasellus mattis sagittis erat quis mollis.</Text>
+          <Text>{aboutMeText}</Text>
           <StaticImage src="../images/fotoCV.png"
             alt="Enrique Kessler Martínez"
             placeholder="blurred"
             layout="constrained"
-                       width={200}
-                       sx={{ float: 'right'}}
+            width={200}
           />
           <Box as='div'>
             <Heading as='h2'>Projects</Heading>
-            <ProjectShow name='testing' description='This is the first project'/>
+            <ProjectShow name='testing' description='This is the first project' />
           </Box>
+          <MagicRainbowText>This is a test</MagicRainbowText>
         </main>
       </Container>
     </Layout>
