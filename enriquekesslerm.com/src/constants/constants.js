bf040eb4a45228f4b54789ff6a91abfae36c9d75
Enrique Kessler Martínez <qkessler@gmail.com>
2021-04-03 09:41:02 +0200

    Adding the use of constants to the /about page

---
+++ enriquekesslerm.com/src/constants/constants.js
@@ -0,0 +1,21 @@
+export const constants = {
+  aboutMeText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna lacus, vestibulum at fermentum a, fermentum ut arcu. Phasellus eget velit est. Vivamus id fringilla ligula, quis pulvinar orci. Morbi dignissim dui libero, at varius justo mollis in. Pellentesque suscipit vitae lorem a volutpat. Phasellus mattis sagittis erat quis mollis.',
+  cqWebDescription: 'Django Web-App for studying which creates an easier customer experience applying a mix between “Flash Cards” and “Active Recall” studying techniques.',
+  appMusicDescription: 'Maven Desktop App created with functionality that allows for xml importing, and implemented with patterns from the GoF Design Patterns book.',
+  timelineElements: [
+    {
+      'year': 2021,
+      'events': [
+        { 'info': '' },
+      ],
+    },
+    {
+      'year': 2020,
+      'events': [
+        { 'info': 'this is the info for the 2020 event' },
+        { 'info': 'this is the second event for 2020' },
+      ],
+    },
+  ],
+
+}
--- enriquekesslerm.com/src/pages/about.js
+++ enriquekesslerm.com/src/pages/about.js
@@ -9,34 +9,11 @@ import ProjectShow from '../components/project-show'
 import Timeline from '../components/timeline'
 import MagicRainbowText from '../components/moving-rainbow-text'
 import CloudQuestionsLogo from '../../assets/images/cq-logo.svg'
-
-const aboutMeText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna lacus, vestibulum at fermentum a, fermentum ut arcu. Phasellus eget velit est. Vivamus id fringilla ligula, quis pulvinar orci. Morbi dignissim dui libero, at varius justo mollis in. Pellentesque suscipit vitae lorem a volutpat. Phasellus mattis sagittis erat quis mollis.'
-
+import { constants } from '../constants/constants'
 
 const whiteFill = {
   fill: 'grey',
 }
-
-const cqWebDescription = 'Django Web-App for studying which creates an easier customer experience applying a mix between “Flash Cards” and “Active Recall” studying techniques.'
-
-const appMusicDescription = 'Maven Desktop App created with functionality that allows for xml importing, and implemented with patterns from the GoF Design Patterns book.'
-
-const timelineElements = [
-  {
-    'year': 2021,
-    'events': [
-      { 'info': 'this is the info for the 2021 event' },
-      { 'info': 'this is the second event for 2021' },
-    ],
-  },
-  {
-    'year': 2020,
-    'events': [
-      { 'info': 'this is the info for the 2020 event' },
-      { 'info': 'this is the second event for 2020' },
-    ],
-  },
-]
 
 export default function AboutPage() {
   return (
@@ -48,7 +25,7 @@ export default function AboutPage() {
             <MagicRainbowText intervalDelay={800}>Enrique</MagicRainbowText>
           </Heading>
           <Box sx={{ display: 'flex' }}>
-            <Text pr={4} sx={{width: '75%'}}>{aboutMeText}</Text>
+            <Text pr={4} sx={{width: '75%'}}>{constants.aboutMeText}</Text>
             <StaticImage src="../../assets/images/fotoCV.png"
               alt="Enrique Kessler Martínez"
               placeholder="blurred"
@@ -58,12 +35,12 @@ export default function AboutPage() {
           </Box>
           <Box as='div' py={4}>
             <Heading as='h2' pb={2}>Projects</Heading>
-            <ProjectShow to='/projects/cloud-questions' name='CloudQuestions web' description={cqWebDescription}>
+            <ProjectShow to='/projects/cloud-questions' name='CloudQuestions web' description={constants.cqWebDescription}>
               <Box as='div' mr={4} ml={1}>
                 <CloudQuestionsLogo width={50} css={whiteFill} />
               </Box>
             </ProjectShow>
-            <ProjectShow to='/projects/app-music' name='AppMusic' description={appMusicDescription}>
+            <ProjectShow to='/projects/app-music' name='AppMusic' description={constants.appMusicDescription}>
               <Box as='div' mr={4} ml={1}>
                 <FaMusic size={40} color='grey' />
               </Box>
@@ -71,7 +48,7 @@ export default function AboutPage() {
           </Box>
           <Box as='div'>
             <Heading as='h2'>Timeline</Heading>
-            <Timeline elements={timelineElements} />
+            <Timeline elements={constants.timelineElements} />
           </Box>
         </main>
       </Container>
