f11170b618a9250ef412f6f5608e43f1859847dc
Enrique Kessler Martínez <qkessler@gmail.com>
2021-04-04 18:59:40 +0200

    Removing imports in the /projects and /404

--- enriquekesslerm.com/src/pages/404.js
+++ enriquekesslerm.com/src/pages/404.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 
-import { Container, Text, Heading, Box, jsx } from 'theme-ui'
+import { Container, Heading, jsx } from 'theme-ui'
 import Layout from 'gatsby-theme-blorg/src/components/layout'
 
 const NotFound = () => (
