afc7ac863bade9682d5949b76e5d5b586639c393
Enrique Kessler Martínez <qkessler@gmail.com>
2021-04-04 18:36:27 +0200

    Adding content variant to /projects and /404

--- enriquekesslerm.com/src/pages/projects.jsx
+++ enriquekesslerm.com/src/pages/projects.jsx
@@ -3,14 +3,14 @@
 import { Container, Text, Heading, Box, jsx } from 'theme-ui'
 import Layout from 'gatsby-theme-blorg/src/components/layout'
 
-const NotFound = () => (
+export default function ProjectsPage() {
+  return (
     <Layout>
       <Container variant='content'>
         <main sx={{ flex: 1, pb: 4, mx: 'auto' }}>
-          <Heading as='h1'>Not found</Heading>
+          <Heading as='h1'>Projects</Heading>
         </main>
       </Container>
     </Layout>
   )
-
-export default NotFound
+}
