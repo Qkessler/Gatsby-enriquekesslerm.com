9cd77c17e91c6955b3bb8f2709b6d198c9a38fb0
Enrique Kessler Martínez <qkessler@gmail.com>
2021-04-05 20:15:45 +0200

    Changing the flex layout to flexBox, configuring the basis and grow

--- enriquekesslerm.com/src/pages/about.jsx
+++ enriquekesslerm.com/src/pages/about.jsx
@@ -20,20 +20,20 @@ export default function AboutPage() {
             <Text sx={{ paddingRight: 2 }}>Hey, It's</Text>
             <MagicRainbowText intervalDelay={800}>Enrique</MagicRainbowText>
           </Heading>
-          <Box sx={{ display: 'flex' }}>
-            <Text pr={4} sx={{width: '75%'}}>{constants.aboutMeText}</Text>
+          <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+            <Text pb={4} pr={4} sx={{ flexGrow: 1, flexBasis: 500 }}>{constants.aboutMeText}</Text>
             <StaticImage src="../../assets/images/fotoCV.png"
               alt="Enrique Kessler Martínez"
               placeholder="blurred"
               layout="constrained"
-              width={200}
+                         sx={{ flexGrow: 1, flexBasis: 0, minWidth: 200, maxWidth: 250, margin: 'auto' }}
             />
           </Box>
           <Box as='div' py={4}>
             <Heading as='h2' pb={2}>Latest Projects</Heading>
-            <Projects number={3}/>
-            <Text>You can find the rest of the projects 
-              <Link as={GatsbyLink} to='/projects' sx={{ paddingLeft: 1}}>here.</Link>
+            <Projects number={3} />
+            <Text>You can find the rest of the projects
+              <Link as={GatsbyLink} to='/projects' sx={{ paddingLeft: 1 }}>here.</Link>
             </Text>
           </Box>
           <Box as='div'>
@@ -42,7 +42,7 @@ export default function AboutPage() {
           </Box>
           <Box as='div'>
             <Heading as='h2' py={4}>Travel map</Heading>
-            <TravelMap center={[37.607152579659946, -0.9878845758906367]} zoom={2} markers={constants.markers}/>
+            <TravelMap center={[37.607152579659946, -0.9878845758906367]} zoom={2} markers={constants.markers} />
           </Box>
         </main>
       </Container>
