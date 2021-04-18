5e728f123e974e042cc67041784e089db31eb9f1
Enrique Kessler Martínez <qkessler@gmail.com>
2021-04-18 20:29:02 +0200

    Adding divider between the elements of the timeline

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -2,12 +2,13 @@
 
 import * as styles from "./timeline.module.css"
 import { Checkbox, Flex, Divider, Box, Text, jsx, Heading } from "theme-ui"
+import { constants } from '../constants/constants'
 
 const TimelineEvent = ({ event, ml, pb }) => {
   return (
     <Flex ml={ml}>
       {event.headline &&
-       <Checkbox defaultChecked={true} />}
+        <Checkbox defaultChecked={true} />}
       <Box pb={pb}>
         <Text as='h5' sx={{ mb: 1 }}>{event.headline}</Text>
         <Text>{event.info}</Text>
@@ -19,13 +20,16 @@ const TimelineEvent = ({ event, ml, pb }) => {
 const Timeline = ({ elements, projects }) => {
   return (
     <Box className={styles.wrapper}>
-      {elements.map(yearEvents => (
+      {elements.map((yearEvents, index) => (
         <Box key={`year-${yearEvents.year}`} as="div">
           <Heading as="h4" pb={projects ? 0 : 3}>{yearEvents.year}</Heading>
           {yearEvents.events.map((event, index) => (
             <TimelineEvent ml={projects ? 0 : 3} pb={projects ? 0 : 3} key={`event-${index}`} event={event} />
           ))}
-          <Divider my={4} />
+          {index != elements.length - 1 && <Divider my={4} sx={{
+            height: '.25em',
+            backgroundColor: 'muted'}} />
+          }
         </Box>
       ))}
     </Box>
